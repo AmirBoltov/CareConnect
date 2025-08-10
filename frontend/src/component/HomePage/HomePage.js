228a3322daa93c95113b777635f9049295fcd9c0
orelsar <oreltz1>
2025-08-10 22:40:52 +0300

    change name of page

---
+++ frontend/src/component/HomePage/HomePage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./HomePage.css";
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">המערכת שלי</div>
+      <ul className="nav-list">
+        <li className="nav-item">בית</li>
+        <li className="nav-item">בקשות</li>
+        <li className="nav-item">מתנדבים</li>
+        <li className="nav-item">אודות</li>
+        <li className="nav-item">צור קשר</li>
+      </ul>
+    </nav>
+  );
+}
+
+function HomePage() {
+  return (
+    <div className="app" dir="rtl">
+      <Navbar />
+      <div className="content">
+        <h1>ברוכים הבאים למערכת הסיוע</h1>
+        <p>כאן תוכלו לפתוח בקשות, להתנדב ולעזור למבוגרים ופצועים בצה"ל.</p>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ frontend/src/component/Login_Page/Login_Page.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Login_Page.css";
+function LoginPage(){
+    
+
+}
+export default LoginPage;
